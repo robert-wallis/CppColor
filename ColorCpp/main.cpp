2e478cc35c1483634fc84d64c28ef013dbd059d5
Robert Wallis <robertw@genui.co>
2019-11-19 20:46:36 -0800

    refactored spans into new files

--- ColorCpp/main.cpp
+++ ColorCpp/main.cpp
@@ -7,35 +7,8 @@
 //
 
 #include <iostream>
-#include <span>
+#include "spans.hpp"
 
-
-void span() {
-    char text[] = "Woah Spans work!";
-    std::span<char> sp(text);
-    for (auto const &c : sp) {
-        std::cout << c;
-    }
-    std::cout << std::endl;
-}
-
-int sum(std::span<int> list) {
-    int total = 0;
-    for (const auto &i : list) {
-        total += i;
-    }
-    return total;
-}
-
-void compile_time_span() {
-    int list[3] = { 1, 2, 3 };
-
-    int total = sum({list, 4}); // should compile time error, 4 > 3 :(
-    std::cout << total << std::endl;
-
-    int total2 = sum(list);
-    std::cout << total2 << std::endl;
-}
 
 int main(int argc, const char * argv[]) {
     span();
---
+++ ColorCpp/spans.hpp
@@ -0,0 +1,19 @@
+//
+//  spans.hpp
+//  ColorCpp
+//
+//  Created by Robert Wallis on 11/19/19.
+//  Copyright © 2019 Robert Wallis. All rights reserved.
+//
+
+#ifndef spans_hpp
+#define spans_hpp
+
+#include <iostream>
+#include <span>
+
+void span();
+int sum(std::span<int> list);
+void compile_time_span();
+
+#endif /* spans_hpp */
