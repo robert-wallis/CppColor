03f7e291ba609424e7e42d8aed88eed0c545426a
Robert Wallis <robertw@genui.co>
2019-11-20 11:59:42 -0800

    P5 RAII file

---
+++ ColorCpp/p8.cpp
@@ -0,0 +1,28 @@
+//
+//  p8.cpp
+//  ColorCpp
+//
+//  Created by Robert Wallis on 11/19/19.
+//  Copyright © 2019 Robert Wallis. All rights reserved.
+//
+
+#include "p8.hpp"
+
+#include <fstream>
+#include <iostream>
+
+#include "LineInFile.hpp"
+
+void p8_raii_file() {
+  std::ifstream file{__FILE__};
+  std::string line;
+  auto in_func = LineInFunction(__FUNCTION__);
+  while (file.good()) {
+    std::getline(file, line);
+    if (in_func(line)) {
+      std::cout << line << std::endl;
+    }
+  }
+}
+
+void other_func() {}
