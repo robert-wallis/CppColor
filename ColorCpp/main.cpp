ed9d3a91942a9a7f60c25c10d762f89a47981aef
Robert Wallis <robertw@genui.co>
2019-11-19 20:15:55 -0800

    trying out static anaylisis of span

--- ColorCpp/main.cpp
+++ ColorCpp/main.cpp
@@ -19,7 +19,26 @@ void span() {
     std::cout << std::endl;
 }
 
+int sum(std::span<int> list) {
+    int total = 0;
+    for (const auto &i : list) {
+        total += i;
+    }
+    return total;
+}
+
+void compile_time_span() {
+    int list[3] = { 1, 2, 3 };
+
+    int total = sum({list, 4}); // should compile time error, 4 > 3 :(
+    std::cout << total << std::endl;
+
+    int total2 = sum(list);
+    std::cout << total2 << std::endl;
+}
+
 int main(int argc, const char * argv[]) {
     span();
+    compile_time_span();
     return 0;
 }
