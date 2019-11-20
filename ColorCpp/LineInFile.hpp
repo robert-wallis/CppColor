0607dffa9a37e9bdd533825c6913c022dfaedabc
Robert Wallis <robertw@genui.co>
2019-11-20 11:57:47 -0800

    keeps track of if the line is in a function

---
+++ ColorCpp/LineInFile.hpp
@@ -0,0 +1,54 @@
+//
+//  LineInFile.hpp
+//  ColorCpp
+//
+//  Created by Robert Wallis on 11/20/19.
+//  Copyright © 2019 Robert Wallis. All rights reserved.
+//
+
+#ifndef LineInFile_h
+#define LineInFile_h
+
+#include <string>
+
+class LineInFunction {
+  std::string function_name;
+  int bracket_level;
+  bool in_this_function;
+
+ public:
+  LineInFunction(const std::string &function_name)
+      : function_name(function_name),
+        bracket_level(0),
+        in_this_function(false) {}
+
+  bool operator()(const std::string &line) {
+    if (!in_this_function) {
+      if (line.find(function_name) != std::string::npos) {
+        in_this_function = true;
+      }
+    }
+    if (in_this_function) {
+      bracket_level += bracket_count(line);
+      if (bracket_level == 0) {
+        in_this_function = false;
+        return true;  // last time it's in the function
+      }
+    }
+    return in_this_function;
+  }
+
+  int static const bracket_count(const std::string &line) {
+    int count = 0;
+    for (const auto &c : line) {
+      if (c == '{') {
+        count++;
+      } else if (c == '}') {
+        count--;
+      }
+    }
+    return count;
+  }
+};
+
+#endif /* LineInFile_h */
