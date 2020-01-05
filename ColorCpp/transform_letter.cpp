55692620b6d1c2a8bd08bc01d5fbf806f43680e2
Robert Wallis <smilingrob@gmail.com>
2020-01-05 14:32:59 -0800

    Messing with std::transform

---
+++ ColorCpp/transform_letter.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Robert Wallis on 1/5/20.
+//
+
+#include <string>
+#include <algorithm>
+#include "gtest/gtest.h"
+
+char nextLetter(char c) {
+  if (c == 'z') return 'a';
+  if (c == 'Z') return 'A';
+  return (char)(c + 1);
+}
+
+std::string mover(std::string word) {
+  std::string moved("");
+  auto result = std::transform(
+      word.begin(),
+      word.end(),
+      std::back_inserter(moved),
+      nextLetter
+  );
+  return moved;
+}
+
+TEST(Transform, Letter) {
+  EXPECT_EQ("bcde", mover("abcd"));
+  EXPECT_EQ("ifmmp", mover("hello"));
+  EXPECT_EQ("czf", mover("bye"));
+  EXPECT_EQ("xfmdpnf", mover("welcome"));
+  EXPECT_EQ("spubufa", mover("rotatez"));
+}
