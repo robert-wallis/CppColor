f3b21dda1597d3373af74ab6673dda37e6b8ade0
Robert Wallis <smilingrob@gmail.com>
2020-01-06 16:58:05 -0800

    Repeating String

---
+++ ColorCpp/double_letter.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Robert Wallis on 1/6/20.
+//
+
+#import <algorithm>
+#import "gtest/gtest.h"
+
+std::string doubleChar(std::string str) {
+  std::string acc;
+  std::for_each(
+      str.begin(), str.end(),
+      [&acc](int c) {
+        acc.push_back(c);
+        acc.push_back(c);
+      }
+  );
+  return acc;
+}
+
+TEST(Edabit, DoubleLetter) {
+  EXPECT_EQ("SSttrriinngg", doubleChar("String"));
+  EXPECT_EQ("HHeelllloo  WWoorrlldd!!", doubleChar("Hello World!"));
+  EXPECT_EQ("11223344!!__  ", doubleChar("1234!_ "));
+}
